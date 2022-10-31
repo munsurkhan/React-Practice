f8d7de15cdf77d2f70b8bf73094cfd4bbd7f57c6
Munsur Rahman <38419661+munsurkhan@users.noreply.github.com>
2022-10-31 23:35:46 +0600

    27
    27

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,30 @@
-import React, {Component} from 'react';
-import HomePage from "./pages/HomePage";
+import React from "react";
+import axios from "axios";
 
-class App extends Component {
 
-    constructor() {
-        super();
+const App = () =>{
+    //post Request
+    function GetList() {
+        let URL = "......."
+        axios.get(URL).then((res)=>{
+            if (res.status===200){
+                let data = res.data;
+            }else{
+                //Something Went Wrong
+            }
+
+        }).catch((err)=>{
+            console.log(err);
+        })
     }
 
-    render() {
-        return (
-            <div>
-                <HomePage/>
-            </div>
-        );
 
 
-    }
-}
 
+    return(
+        <div>
+
+        </div>
+    );
+};
 export default App;
