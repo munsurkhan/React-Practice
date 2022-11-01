3d631775067582994694910fffe1d539e4c4da0f
Munsur Rahman <munsurinfo@gmail.com>
2022-11-01 19:25:32 +0600

    com

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import axios from "axios";
+import cogoToast from "cogo-toast";
 
 
 const App = () =>{
-    //post Request
-    function GetList() {
-        let URL = "......."
-        axios.get(URL).then((res)=>{
-            if (res.status===200){
-                let data = res.data;
-            }else{
-                //Something Went Wrong
-            }
+    const ErrorToast = () => {
+      cogoToast.error("This is Error Message");
+    }
 
-        }).catch((err)=>{
-            console.log(err);
-        })
+    const SuccessToast = () => {
+        cogoToast.success("This is Success Message");
     }
 
 
@@ -23,7 +17,8 @@ const App = () =>{
 
     return(
         <div>
-
+            <button onClick={ErrorToast}>Error</button>
+            <button onClick={SuccessToast}>Success</button>
         </div>
     );
 };
